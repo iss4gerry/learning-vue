ee29f990fcfe2a3343889ba5b9878b8895164bfd
gerry <gerrydesrian488@gmail.com>
2024-08-17 12:00:49 +0700

    conditional, looping, user input, lifecycle hooks

---
+++ part-1/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        maximum: 50,
+        products: null
+    },
+    mounted: function(){
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(res => res.json())
+        .then(data => {
+            this.products = data
+        })
+    }
+});
