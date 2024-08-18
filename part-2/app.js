d93112840b0664dd41e984d67f1c0ef36575e952
gerry <gerrydesrian488@gmail.com>
2024-08-18 12:40:39 +0700

    difference between computed & methods

--- part-2/app.js
+++ part-2/app.js
@@ -4,11 +4,29 @@ var app = new Vue({
         slugText: 'The Quick #(*!&@ Brown 29'
     },
     computed: { 
+        now: function (){
+            var date = new Date()
+            return (
+                String(date.getHours()) + 
+                String(date.getMinutes()) + 
+                String(date.getSeconds()) 
+            )
+        },
         slugetize: function () {
             return this.slugText
                 .toLowerCase()
                 .replace(/[^\w ]+/g, '')
-                .replace(/ +/g, '-');
-        }
+                .replace(/ +/g, '-') + '-' + this.now;
+        },
     },
+    methods: {
+        // now: function (){
+        //     var date = new Date()
+        //     return (
+        //         String(date.getHours()) + 
+        //         String(date.getMinutes()) + 
+        //         String(date.getSeconds()) 
+        //     )
+        // }
+    }
 });
